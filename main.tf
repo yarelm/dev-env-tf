135d912edcf4b43b695f91f643f589e58cc8808e
Yarel Maman <yarel@doit-intl.com>
2021-01-07 11:03:27 +0200

    test

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
 module "db" {
+    google_credentials = var.google_credentials
     source = "./modules/db"
 }
---
+++ modules/db/variables.tf
@@ -0,0 +1,4 @@
+variable "google_credentials" {
+  description = "the contents of a service account key file in JSON format."
+  type = string
+}
